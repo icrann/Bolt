5217f21c71e37cc2a664ab1c3096a559603099ec
crannm <62957317+crannm@users.noreply.github.com>
2021-06-23 14:28:20 +0100

    Bolt.lua 21.0.12
    Added Bolt.ChangeLeaderstat for changing leaderstats easily in the game.

--- Bolt.lua
+++ Bolt.lua
@@ -1,7 +1,7 @@
 local Bolt = {}
 
-Bolt.Version = "21.0.11"
-Bolt.VersionDetails = "Bolt 21.0.11: Added Bolt.NewLeaderstat for adding leaderstats easily to the game. Also Bolt.Leaderstat for accessing the leaderstat folder"
+Bolt.Version = "21.0.12"
+Bolt.VersionDetails = "Bolt 21.0.12: Added Bolt.ChangeLeaderstat for changing leaderstats easily in the game."
 
 
 game.Players.PlayerAdded:Connect(function(player)
@@ -18,9 +18,15 @@ function Bolt.RemoveHealth(player, health)
 	player.Character:WaitForChild("Humanoid").Health = (player.Character:WaitForChild("Humanoid").Health - health)
 end
 
-function Bolt.NewLeaderstat(player, name, variableType)
+function Bolt.NewLeaderstat(player, name, variableType, value)
 	local value = Instance.new(variableType, player:WaitForChild("leaderstats"))
 	value.Name = name
+	value.Value = value
+end
+
+function Bolt.ChangeLeaderstat(player, name, value)
+	local stat = player:WaitForChild("leaderstats"):WaitForChild(name)
+	stat.Value = value
 end
 
 
