5b14570e0d9bfcca905006bfba70c8a370979479
crannm <62957317+crannm@users.noreply.github.com>
2021-07-04 15:36:34 +0100

    Bolt 21.0.1.4
    Added Dark and Light Bubble Chat. Using Bolt.Dark/LightBubbleChat. Also Bolt.AwardBadge was added.

--- Bolt.lua
+++ Bolt.lua
@@ -2,9 +2,12 @@ local Bolt = {}
 
 local players = game:GetService("Players")
 local marketplace = game:GetService("MarketplaceService")
+local datastores = game:GetService("DataStoreService")
+local badges = game:GetService("BadgeService")
+local chat = game:GetService("Chat")
 
-Bolt.Version = "21.0.13"
-Bolt.VersionDetails = "Bolt 21.0.12: Added Bolt.ChangeLeaderstat for changing leaderstats easily in the game."
+Bolt.Version = "21.0.14"
+Bolt.VersionDetails = "Bolt 21.0.14: Added Dark and Light Bubble Chat. Using Bolt.Dark/LightBubbleChat. Also Bolt.AwardBadge was added."
 
 
 game.Players.PlayerAdded:Connect(function(player)
@@ -34,6 +37,28 @@ end
 
 function Bolt.PromptPremium(player)
 	marketplace:PromptPremiumPurchase(player)
+end
+
+function Bolt.AwardBadge(playerId, badgeId)
+	badges:AwardBadge(playerId, badgeId)
+end
+
+function Bolt.DarkBubbleChat()
+	local BubbleChatSettings = {
+		BackgroundColor3 = Color3.fromRGB(50, 50, 50),
+		TextColor3 = Color3.fromRGB(255, 255, 255)
+	}
+	
+	chat:SetBubbleChatSettings(BubbleChatSettings)
+end
+
+function Bolt.LightBubbleChat()
+	local BubbleChatSettings = {
+		BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+		TextColor3 = Color3.fromRGB(0, 0, 0)
+	}
+
+	chat:SetBubbleChatSettings(BubbleChatSettings)
 end
 
 
